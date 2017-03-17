88b63aada6f31c0f3c7e6ab1c257e69b55c5e8bf
Victor Torres <victor@adroll.com>
2017-03-17 17:33:31 -0400

    testing out api calls from ImageDropzone

--- app/components/ImageDropzone.jsx
+++ app/components/ImageDropzone.jsx
@@ -1,8 +1,24 @@
-var React = require('react');
-var Dropzone = require('react-dropzone');
+import React from 'react';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
 
 var DropzoneDemo = React.createClass({
+
+  getInitialState: function() {
+    return {
+      testInitialState1: ''
+    }
+  },
     onDrop: function (acceptedFiles, rejectedFiles) {
+      request
+        .get('https://api.github.com/repos/facebook/react/commits')
+        .end( (err, res) => {
+          console.log("attempting to set state for testInitialState1");
+          this.setState({testInitialState1: res});
+          console.log("attempted to set state for testInitialState1");
+          console.log(this.state);
+        })
+      console.log(this.state);
       console.log('Accepted files: ', acceptedFiles);
       console.log('Rejected files: ', rejectedFiles);
     },

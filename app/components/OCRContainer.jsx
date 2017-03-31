cfd940742d4c17595a55aa4121f1b382e9ed6f6f
Victor Torres <victor@adroll.com>
2017-03-30 23:36:32 -0400

    OCR process on drop instead of via button click (killed button entirely)

--- app/components/OCRContainer.jsx
+++ app/components/OCRContainer.jsx
@@ -9,17 +9,13 @@ var OCRContainer = React.createClass({
     return { imgString: "" }
   },
 
-  onButtonClick: function() {
-    console.log("process image here so that we can get the OCR representation");
-    // process file -> OCR
-    if (this.props.uploadedFile === ""){
-      // TODO: Add bootsrap alert here
-      console.log("there is no file detected");
-    } else {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.uploadedFile) {
+      console.log("process image here so that we can get the OCR representation");
+      // process file -> OCR
       console.log("Should be a file here...");
-      console.log(this.props.uploadedFile);
       let upload = request.post(this.props.apiVersion + this.props.processImageRoute)
-                          .field('file', this.props.uploadedFile);
+                          .field('file', nextProps.uploadedFile);
 
       upload.end((err, response) => {
         if (err) {
